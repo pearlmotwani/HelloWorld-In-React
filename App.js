e824b0cb66d771ca68c4eb3e391b6e6bac00e4cf
Pearl <69087040+pearlmotwani@users.noreply.github.com>
2021-07-13 14:40:21 +0530

    Day 1 Assignment Submission

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+const Apple=()=> {
+    return (
+        <div>
+            <h1 style={{textAlign :"center"}}>Hello Everyone</h1>
+            <h2>Click on which language you're comfortable in</h2>
+            <Button/>
+        </div>
+
+    )
+}
+
+export default Apple
---
+++ Button.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Button=()=>{
+    return(
+        <div>
+            <button className="button"> French</button>
+            <button className="button"> German</button>
+            <button className="button">English</button>
+        </div>
+    
+    )
+    
+}
+export default Button 
